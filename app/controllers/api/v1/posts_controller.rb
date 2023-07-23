8849d1ce8648bec5d91bdd107dca096fcc41ee04
yoshi <mdmdmdmdmd57@gmail.com>
2023-07-23 22:22:11 +0900

    index,show,createアクションの中身を実装

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,16 +1,30 @@
 class Api::V1::PostsController < ApplicationController
   def index
+    @posts = Post.all
+    render json: @posts
   end
 
   def show
+    @post = Post.find(params[:id])
+    render json: @post
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors status: :unprocessable_entitiy
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title,:content)
   end
 end
